37c1b41d1c63947447ab1bfa3a7cfdfb237b42d0
Stephen Foster <stephen@thoughtstem.com>
2022-01-21 15:09:36 -0800

    footer

--- src/App.js
+++ src/App.js
@@ -133,17 +133,9 @@ function MainContent(){
   )
 }
 
-function NavBar(){
+function SocialMediaLinks(){
 
-  return(
-  <AppBar position="static" color="primary" >
-    <Container maxWidth="xl">
-    <Toolbar disableGutters>
-      <a href="https://codespells.org">
-        <img src="./logo.png" style={{width: 100}}/> 
-      </a>
-      <Box sx={{ flexGrow: 1 }}></Box>
-      <Box sx={{ flexGrow: 0 }}>
+ return (<>
         <a style={{color: "rgba(255,255,255,.5)"}} 
           target="_blank" 
           href="https://www.twitch.tv/codespells" 
@@ -164,6 +156,21 @@ function NavBar(){
           target="_blank" 
           href="https://discord.gg/prsZZnm" 
           className="fab fa-discord fa-2x p-1"></a>
+  </>)
+}
+
+function NavBar(){
+
+  return(
+  <AppBar position="static" color="primary" >
+    <Container maxWidth="xl">
+    <Toolbar disableGutters>
+      <a href="https://codespells.org">
+        <img src="./logo.png" style={{width: 100}}/> 
+      </a>
+      <Box sx={{ flexGrow: 1 }}></Box>
+      <Box sx={{ flexGrow: 0 }}>
+        <SocialMediaLinks />
       </Box>
     </Toolbar>
     </Container>
@@ -171,12 +178,71 @@ function NavBar(){
   )
 }
 
+function FooterLink(props){
+   return <a style={{display: "block", marginBottom: 10,  textDecoration: "none", color: "#007bff"}} href={props.href}>{props.children}</a>
+}
+
+function SupportUs(){
+ return (<>
+   <h3 style={{color: "white"}}>Support Us</h3>
+   <FooterLink href="https://patreon.com/codespells">Join Our Patreon</FooterLink>
+   <FooterLink href="https://twitch.tv/codespells">Become a Twitch Subscriber</FooterLink>
+   <FooterLink href="https://store.steampowered.com/app/324190/CodeSpells/">Purchase Previous Version on Steam</FooterLink>
+   <FooterLink href="https://codespells.us17.list-manage.com/subscribe?u=dbaae34bac76f17fad7bc6efd&id=a0f4e4cdc2">Join Our Mailing List</FooterLink>
+ </>)
+}
+
+function ContactUs(){
+ return (<>
+   <h3 style={{color: "white"}}>Contact Us</h3>
+   <p>codespells [at] thoughtstem [dot] com</p>
+</>)
+}
+
+function FollowUs(){
+
+ return (<>
+   <h3 style={{color: "white"}}>Follow Us</h3>
+   <SocialMediaLinks />
+</>)
+}
+
+function Copyright(){
+
+ return (<>
+   <br/>
+   <br/>
+   <p>ThoughtSTEM, LLC Copyright 2022</p>
+   <p>All Rights Reserved</p>
+ </>)
+}
+
+function Footer() {
+  return (
+   <footer style={{zIndex: 1, position: "relative", margin: 0,  width: "100vw", backgroundColor: "#343a40", paddingBottom: 20, marginTop: 250, color: "white"}}>
+    <Container maxWidth="md">
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+           <SupportUs />
+           <ContactUs />
+        </Grid>
+        <Grid item xs={6}>
+           <FollowUs />
+           <Copyright />
+        </Grid>
+      </Grid>
+    </Container>
+   </footer>
+  )
+}
+
 function App() {
   return (
     <>
-      <NavBar/>
       <BackgroundImage/>
+      <NavBar/>
       <MainContent/>
+      <Footer/>
     </>
   );
 }
